220e5f23a63465a9291b98cd184d5dfbe177d3bb
Anwesh43 <anweshthecool0@gmail.com>
2018-07-28 02:58:22 +0530

    adding LRRRView class

--- lrrrview/src/main/java/com/anwesh/uiprojects/lrrrview/LRRRView.kt
+++ lrrrview/src/main/java/com/anwesh/uiprojects/lrrrview/LRRRView.kt
@@ -10,3 +10,23 @@ import android.view.MotionEvent
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.RectF
+
+val nodes : Int = 5
+
+class LRRRView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
