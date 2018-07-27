682ed43dceef6ca9863ae0642363b72a22d4e02a
Anwesh43 <anweshthecool0@gmail.com>
2018-07-28 02:56:22 +0530

    adding lrrrview module

---
+++ lrrrview/src/main/java/com/anwesh/uiprojects/lrrrview/LRRRView.kt
@@ -0,0 +1,12 @@
+package com.anwesh.uiprojects.lrrrview
+
+/**
+ * Created by anweshmishra on 28/07/18.
+ */
+
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
