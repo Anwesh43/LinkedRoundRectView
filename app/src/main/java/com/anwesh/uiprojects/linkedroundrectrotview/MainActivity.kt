a0ea2a581e6341f93172f7158d57dab9cb8055fa
Anwesh43 <anweshthecool0@gmail.com>
2018-07-28 03:25:54 +0530

    using view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedroundrectrotview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedroundrectrotview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.linkedroundrectrotview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.lrrrview.LRRRView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LRRRView.create(this)
     }
 }
