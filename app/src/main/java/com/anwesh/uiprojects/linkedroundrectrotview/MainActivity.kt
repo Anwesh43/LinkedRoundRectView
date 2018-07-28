758c9311de80b61d478b8864bda948ba22ba7fd7
Anwesh43 <anweshthecool0@gmail.com>
2018-07-28 12:00:06 +0530

    adding AnimationListener in Acticvity

--- app/src/main/java/com/anwesh/uiprojects/linkedroundrectrotview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedroundrectrotview/MainActivity.kt
@@ -3,15 +3,26 @@ package com.anwesh.uiprojects.linkedroundrectrotview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.lrrrview.LRRRView
 
 class MainActivity : AppCompatActivity() {
 
+    private val completeListener : (Int) -> Unit = { createToast("completed animation ${it + 1}")}
+
+    private val resetListener : (Int) -> Unit = {createToast("animation is reset ${it + 1}")}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LRRRView.create(this)
+        val view : LRRRView = LRRRView.create(this)
         fullScreen()
+        view.addAnimationListener(completeListener, resetListener)
     }
+
+    private fun createToast(msg : String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
 }
 
 fun MainActivity.fullScreen() {
